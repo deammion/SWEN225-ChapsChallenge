e1000bd452854e1bce4f6120c2afaab2c34411c6
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-15 00:04:01 +1200

    Create Game class
    Game class with basic skeletal methods

---
+++ src/nz/ac/vuw/ecs/swen225/gp30/maze/Game.java
@@ -0,0 +1,18 @@
+package nz.ac.vuw.ecs.swen225.gp30.maze;
+
+public class Game {
+    private boolean gamePaused;
+    // Level level
+    
+    public Game() { }
+
+    public void move() { }
+
+    public void resumeGame() { }
+
+    public void pauseGame() { }
+
+    public void saveGame() { }
+
+    public void exitGame() { }
+}
