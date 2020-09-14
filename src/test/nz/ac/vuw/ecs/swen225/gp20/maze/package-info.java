f1b896d9e8c606e850b0e5825abd4930ca34e949
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-14 19:50:40 +1200

    Created test packages
    Test packages for maze and persistence packages. Added info for packages

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp20/maze/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Test package for maze
+ *
+ * @Author Oscar Camplin
+ */
+package test.nz.ac.vuw.ecs.swen225.gp20.maze;
---
+++ src/test/nz/ac/vuw/ecs/swen225/gp20/persistence/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Test package for persistence
+ *
+ * @Author
+ */
+package test.nz.ac.vuw.ecs.swen225.gp20.persistence;
