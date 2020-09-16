b84fbc16350302ddc7fcfc2d71212c41af31ee01
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-16 14:59:51 +1200

    Refactored game state

--- src/test/nz/ac/vuw/ecs/swen225/gp30/maze/ChapTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp30/maze/ChapTest.java
@@ -2,33 +2,47 @@ package nz.ac.vuw.ecs.swen225.gp30.maze;
 
 import nz.ac.vuw.ecs.swen225.gp30.maze.item.Key;
 import nz.ac.vuw.ecs.swen225.gp30.maze.tile.FreeTile;
+import nz.ac.vuw.ecs.swen225.gp30.maze.tile.Tile;
 import nz.ac.vuw.ecs.swen225.gp30.maze.tile.WallTile;
 import org.junit.jupiter.api.Test;
 
 public class ChapTest {
-    @Test
-    public void testAddAndRemoveInventory() { }
 
     @Test
-    public void testMoveUp() {
+    public void validMoveTest_01() { // move chap onto wall tile
         Game game = new Game();
         Chap chap = new Chap();
-        Maze maze = new Maze(2, 2);
+        Maze maze = makeTestMaze(new FreeTile(1, 0));
 
-
-        for(int x=0; x<2; x++) {
-            for(int y=0; y<2; y++) {
-                maze.setTileAt(x, y, new FreeTile(x, y));
-            }
-        }
-        chap.setX(0);
-        chap.setY(0);
         game.setChap(chap);
         game.setMaze(maze);
+        game.setChapOnMaze(0,0);
 
-        game.move(Move.UP);
-        assert(maze.getTileAt(0, 1).hasChap());
+        game.moveChap(Move.RIGHT);
+        assert(maze.getTileAt(1, 0).hasChap());
         assert(!maze.getTileAt(0, 0).hasChap());
-        assert(chap.getX() == 0 && chap.getY() == 1);
+    }
+
+    @Test
+    public void invalidMoveTest_01() { // move chap onto wall tile
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new WallTile(1, 0));
+
+        game.setChap(chap);
+        game.setMaze(maze);
+        game.setChapOnMaze(0,0);
+
+        game.moveChap(Move.RIGHT);
+        assert(!maze.getTileAt(1, 0).hasChap());
+        assert(maze.getTileAt(0, 0).hasChap());
+    }
+
+
+    public Maze makeTestMaze(Tile testTile) {
+        Maze maze = new Maze(2, 1);
+        maze.setTileAt(0,0, new FreeTile(0, 0));
+        maze.setTileAt(1,0, testTile);
+        return maze;
     }
 }
