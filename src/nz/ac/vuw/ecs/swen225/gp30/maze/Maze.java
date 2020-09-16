4c3cc04327293aff7023b44554c19b4b55b62c56
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-16 15:01:02 +1200

    Fixed row and col discrepancy bug in maze
    x and y values were the wrong way around

--- src/nz/ac/vuw/ecs/swen225/gp30/maze/Maze.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/maze/Maze.java
@@ -5,20 +5,21 @@ import nz.ac.vuw.ecs.swen225.gp30.maze.tile.Tile;
 public class Maze {
     private Tile[][] grid;
 
-    public Maze(int rows, int cols) {
-        grid = new Tile[rows][cols];
+    public Maze(int cols, int rows) {
+        grid = new Tile[cols][rows];
     }
 
     public void setTileAt(int x, int y, Tile tile) {
-        checkArgument(x >= 0 && x < grid.length, "x must be within bounds: 0 - " + grid.length);
-        checkArgument(y >= 0 && y < grid[0].length, "y must be within bounds: 0 - " + grid[0].length);
+        checkArgument(x >= 0 && x < grid.length, "x (" + x + ") must be within bounds: 0 - " + (grid.length-1));
+        checkArgument(y >= 0 && y < grid[0].length, "y (" + y + ") must be within bounds: 0 - " + (grid[0].length-1));
         checkArgument(tile != null, "tile must not be null");
         grid[x][y] = tile;
     }
 
     public Tile getTileAt(int x, int y) {
-        checkArgument(x >= 0 && x < grid.length, "x must be within bounds: 0 - " + grid.length);
-        checkArgument(y >= 0 && y < grid[0].length, "y must be within bounds: 0 - " + grid[0].length);
+        checkArgument(x >= 0 && x < grid.length, "x (" + x + ") must be within bounds: 0 - " + (grid.length-1));
+        checkArgument(y >= 0 && y < grid[0].length, "y (" + y + ") must be within bounds: 0 - " + (grid[0].length-1));
         return grid[x][y];
     }
+
 }
