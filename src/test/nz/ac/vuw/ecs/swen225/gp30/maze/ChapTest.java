f379dd5d3ec16dc2678b94450fc1c96118c85cf6
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-21 12:38:34 +1200

    Added chip collection test

--- src/test/nz/ac/vuw/ecs/swen225/gp30/maze/ChapTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp30/maze/ChapTest.java
@@ -1,6 +1,7 @@
 package nz.ac.vuw.ecs.swen225.gp30.maze;
 
 import nz.ac.vuw.ecs.swen225.gp30.maze.item.Key;
+import nz.ac.vuw.ecs.swen225.gp30.maze.tile.ExitLockTile;
 import nz.ac.vuw.ecs.swen225.gp30.maze.tile.FreeTile;
 import nz.ac.vuw.ecs.swen225.gp30.maze.tile.Tile;
 import nz.ac.vuw.ecs.swen225.gp30.maze.tile.WallTile;
@@ -38,6 +39,19 @@ public class ChapTest {
         assert(maze.getTileAt(0, 0).hasChap());
     }
 
+    public void validChipCollectionTest_01() {
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new ExitLockTile(1, 0));
+
+        game.setChap(chap);
+        game.setMaze(maze);
+        game.setChapOnMaze(0,0);
+        game.moveChap(Move.RIGHT);
+
+        assert(chap.getChipsCollected() == 1);
+        assert(maze.getTileAt(1, 0) instanceof FreeTile);
+    }
 
     public Maze makeTestMaze(Tile testTile) {
         Maze maze = new Maze(2, 1);
