1562f3a4599cc1cc93f40507e7ee33eb0d73ea52
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-21 12:37:42 +1200

    Fixed movement bug
    Movement in wrong direction bug

--- src/nz/ac/vuw/ecs/swen225/gp30/maze/Game.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/maze/Game.java
@@ -1,16 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp30.maze;
 
+import com.google.common.base.Preconditions;
 import nz.ac.vuw.ecs.swen225.gp30.maze.tile.Tile;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
 public class Game {
-    private enum GameState {
-        RUNNING,
-        INFO,
-        PAUSED
-    }
-
     GameState state;
     Chap gameChap;
     Maze gameMaze;
@@ -21,33 +16,32 @@ public class Game {
     }
 
     public void run() {
-        // checkArgument(level != null);
         checkArgument(gameChap != null, "chap cannot be null");
-        if(state == GameState.PAUSED) { return; }
-
-
+        if(state == GameState.PAUSED) {
+            return;
+        }
     }
 
     public Tile getNewTile(Move move, int oldX, int oldY) {
         switch(move) {
             case UP: return gameMaze.getTileAt(oldX, oldY+1);
             case DOWN: return gameMaze.getTileAt(oldX, oldY-1);
-            case LEFT: return gameMaze.getTileAt(oldX-1, oldX+1);
-            case RIGHT: return gameMaze.getTileAt(oldX+1, oldY+1);
+            case LEFT: return gameMaze.getTileAt(oldX-1, oldY);
+            case RIGHT: return gameMaze.getTileAt(oldX+1, oldY);
         }
         return null;
     }
 
-    public void move(Move move) {
+    public void moveChap(Move move) {
         int oldX = gameChap.getX();
         int oldY = gameChap.getY();
         Tile oldTile = gameMaze.getTileAt(oldX, oldY);
         Tile newTile = getNewTile(move, oldX, oldY);
 
-        oldTile.removeChap();
-        newTile.addChap(gameChap);
-        gameChap.setX(newTile.getX());
-        gameChap.setY(newTile.getY());
+        if(newTile.canMoveTo()) {
+            oldTile.removeChap();
+            setChapOnMaze(newTile.getX(), newTile.getY());
+        }
     }
 
     public void resumeGame() {
@@ -60,15 +54,25 @@ public class Game {
 
     public void saveGame() { }
 
-    public void exitGame() {  }
+    public void exitGame() { }
 
-    public void setChap(Chap chap) { gameChap = chap; }
+    public void setChap(Chap chap) {
+        gameChap = chap;
+    }
+
+    public void setChapOnMaze(int x, int y) {
+        Preconditions.checkArgument(gameChap != null, "chap cannot be null");
+        Tile tile = gameMaze.getTileAt(x, y);
+        tile.addChap(gameChap);
+    }
 
     public Chap getChap() {
         return gameChap;
     }
 
-    public void setMaze(Maze maze) { gameMaze = maze; }
+    public void setMaze(Maze maze) {
+        gameMaze = maze;
+    }
 
     public Maze getMaze() {
         return gameMaze;
