689056ca9736ba5913562b50828a2d1885660112
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-25 13:03:13 +1200

    Created Item enum
    Item enum to store items (e.g. keys)

--- src/nz/ac/vuw/ecs/swen225/gp30/maze/item/Item.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/maze/item/Item.java
@@ -1,4 +1,17 @@
 package nz.ac.vuw.ecs.swen225.gp30.maze.item;
 
-public interface Item {
+public enum Item {
+    BLUE_KEY(ItemType.KEY),
+    GREEN_KEY(ItemType.KEY),
+    YELLOW_KEY(ItemType.KEY);
+
+    public final ItemType type;
+
+    private Item(ItemType type) {
+        this.type = type;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
 }
