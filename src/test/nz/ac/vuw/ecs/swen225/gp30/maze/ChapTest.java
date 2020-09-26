10a893a96c198dd7205ccb69fa60981c2635323e
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-09-26 13:03:15 +1200

    Added tests for movement onto tiles

--- src/test/nz/ac/vuw/ecs/swen225/gp30/maze/ChapTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp30/maze/ChapTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ChapTest {
 
     @Test
-    public void validMoveTest_01() { // move chap onto wall tile
+    public void validMoveTest_01() { // move chap onto free tile
         Game game = new Game();
         Chap chap = new Chap();
         Maze maze = makeTestMaze(new FreeTile(1, 0));
@@ -22,7 +22,110 @@ public class ChapTest {
     }
 
     @Test
-    public void invalidMoveTest_01() { // move chap onto wall tile
+    public void validMoveTest_02() { // move chap onto info tile
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new InfoTile(1, 0));
+        setupGame(game, maze, chap);
+
+        String[] expected = { "|c|i|", "|_|c|", "|c|i|" };
+        assertEquals(expected[0], maze.toString());
+        game.moveChap(Move.RIGHT);
+        assertEquals(expected[1], maze.toString());
+        game.moveChap(Move.LEFT);
+        assertEquals(expected[2], maze.toString());
+    }
+
+    @Test
+    public void validMoveTest_03() { // move chap onto exit tile
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new ExitTile(1, 0));
+        setupGame(game, maze, chap);
+
+        String[] expected = { "|_|c|", "|c|O|" };
+
+        game.moveChap(Move.RIGHT);
+        assertEquals(expected[0], maze.toString());
+        game.moveChap(Move.LEFT);
+        assertEquals(expected[1], maze.toString());
+    }
+
+    @Test
+    public void validMoveTest_04() { // move chap onto locked door tile
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new LockedDoorTile(1, 0, Item.BLUE_KEY));
+        setupGame(game, maze, chap);
+        chap.addItemToInventory(Item.BLUE_KEY);
+
+        String[] expected = { "|c|D|", "|_|c|", "|c|_|" };
+        assertEquals(expected[0], maze.toString());
+
+        game.moveChap(Move.RIGHT);
+        assertEquals(expected[1], maze.toString());
+
+        game.moveChap(Move.LEFT);
+        assertEquals(expected[2], maze.toString());
+    }
+
+    @Test
+    public void validMoveTest_05() { // move chap to key tile and collect key
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new KeyTile(1, 0, Item.BLUE_KEY));
+        setupGame(game, maze, chap);
+
+        String[] expected = { "|c|%|", "|_|c|", "|c|_|" };
+        assertEquals(expected[0], maze.toString());
+
+        game.moveChap(Move.RIGHT);
+        assert(chap.getInventory().size() == 1);
+        assertEquals(expected[1], maze.toString());
+
+        game.moveChap(Move.LEFT);
+        assertEquals(expected[2], maze.toString());
+    }
+
+    @Test
+    public void validMoveTest_06() { // move chap onto treasure tile and collect treasure
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new TreasureTile(1, 0));
+        setupGame(game, maze, chap);
+
+        String[] expected = { "|c|*|", "|_|c|", "|c|_|" };
+        assertEquals(expected[0], maze.toString());
+
+        game.moveChap(Move.RIGHT);
+        assert(chap.getChipsCollected() == 1);
+        assertEquals(expected[1], maze.toString());
+
+        game.moveChap(Move.LEFT);
+        assertEquals(expected[2], maze.toString());
+    }
+
+    @Test
+    public void validMoveTest_07() { // move chap onto exit lock tile (with required treasure #)
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new ExitLockTile(1, 0, 1));
+        setupGame(game, maze, chap);
+
+        String[] expected = { "|c|X|", "|_|c|", "|c|_|" };
+
+        assertEquals(expected[0], maze.toString());
+
+        chap.collectChip();
+        game.moveChap(Move.RIGHT);
+        assertEquals(expected[1], maze.toString());
+
+        game.moveChap(Move.LEFT);
+        assertEquals(expected[2], maze.toString());
+    }
+
+    @Test
+    public void invalidMoveTest_01() { // try move chap onto wall tile
         Game game = new Game();
         Chap chap = new Chap();
         Maze maze = makeTestMaze(new WallTile(1, 0));
@@ -34,50 +137,7 @@ public class ChapTest {
         assertEquals(expected, maze.toString());
     }
 
-    @Test
-    public void validChipCollectionTest_01() {
-        Game game = new Game();
-        Chap chap = new Chap();
-        Maze maze = makeTestMaze(new TreasureTile(1, 0));
-        setupGame(game, maze, chap);
-
-        String expected = "|_|c|";
-        game.moveChap(Move.RIGHT);
-
-        assert(chap.getChipsCollected() == 1);
-        assertEquals(expected, maze.toString());
-    }
-
-    @Test
-    public void validKeyCollectionTest_01() {
-        Game game = new Game();
-        Chap chap = new Chap();
-        Maze maze = makeTestMaze(new KeyTile(1, 0, Item.BLUE_KEY));
-        setupGame(game, maze, chap);
-
-        String expected = "|_|c|";
-        game.moveChap(Move.RIGHT);
-
-        assert(chap.getInventory().size() == 1);
-        assertEquals(expected, maze.toString());
-    }
-
-    @Test
-    public void validUnlockDoorTest_01() {
-        Game game = new Game();
-        Chap chap = new Chap();
-        Maze maze = makeTestMaze(new LockedDoorTile(1, 0, Item.BLUE_KEY));
-        setupGame(game, maze, chap);
-
-        chap.addItemToInventory(Item.BLUE_KEY);
-        game.moveChap(Move.RIGHT);
-
-        String expected = "|_|c|";
-
-        assertEquals(expected, maze.toString());
-    }
-
-    @Test void invalidUnlockDoorTest_01() {
+    @Test void invalidMoveTest_02() { // try move chap to locked door tile without key
         Game game = new Game();
         Chap chap = new Chap();
         Maze maze = makeTestMaze(new LockedDoorTile(1, 0, Item.BLUE_KEY));
@@ -89,6 +149,17 @@ public class ChapTest {
         assertEquals(expected, maze.toString());
     }
 
+    @Test void invalidMoveTest_03() { // try move chap to exit lock tile without enough chips
+        Game game = new Game();
+        Chap chap = new Chap();
+        Maze maze = makeTestMaze(new ExitLockTile(1, 0, 1));
+        setupGame(game, maze, chap);
+
+        String expected = "|c|X|";
+
+        game.moveChap(Move.RIGHT);
+        assertEquals(expected, maze.toString());
+    }
 
     /* HELPER METHODS */
 
