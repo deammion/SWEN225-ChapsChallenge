057bf395517071fb45f4c04456db76a7ea7e9a71
gohilpran <gohilpran@myvuw.ac.nz>
2020-09-30 23:32:50 +1300

    Update to GameVisuals

--- src/nz/ac/vuw/ecs/swen225/gp30/render/GameVisuals.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/render/GameVisuals.java
@@ -5,16 +5,15 @@ import javax.swing.*;
 
 import nz.ac.vuw.ecs.swen225.gp30.maze.GameObject;
 import nz.ac.vuw.ecs.swen225.gp30.maze.GameWorld;
-import nz.ac.vuw.ecs.swen225.gp30.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp30.maze.tile.Tile;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -23,44 +22,37 @@ public class GameVisuals extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	/*
-	private BufferedImage tile_exit;
-	private BufferedImage tile_exit_lock;
-	private BufferedImage tile_free;
-	private BufferedImage tile_info;
-	private BufferedImage tile_treasure;
-	private BufferedImage tile_wall;
-	private BufferedImage key_blue;
-	private BufferedImage key_green;
-	private BufferedImage key_red;
-	private BufferedImage key_yellow;
-	private BufferedImage locked_blue;
-	private BufferedImage locked_green;
-	private BufferedImage locked_red;
-	private BufferedImage locked_yellow;
-	*/
 	
 	private GameWorld game;
-	Stream<Tile> allTiles;
 	private final int TILE_SIZE = 42;
 	private final int CAMERA_VIEW = 9;
+	private final Color BG_COLOR = new Color(144, 164, 174);
 	
 	public GameVisuals(GameWorld game) {
 		this.game = game;
-		this.setSize(new Dimension(TILE_SIZE*CAMERA_VIEW, TILE_SIZE*CAMERA_VIEW));
-		JFrame j = new JFrame();
-		j.setLayout(new FlowLayout());
-		j.add(this);
-		j.pack();
+		this.setPreferredSize((new Dimension(TILE_SIZE*CAMERA_VIEW, TILE_SIZE*CAMERA_VIEW)));
+		this.setBorder(BorderFactory.createRaisedBevelBorder());
 	}
-	
-	public List<Tile> getTilesToRender() {
-		return game.getMaze().getTiles().collect(Collectors.toList());
+
+	public Collection<Tile> getTilesToRender() {
+		GameObject c = game.getChap();
+		int cX = c.getX();
+		int cY = c.getY();
+		int lowerX = cX - CAMERA_VIEW/2;
+		int upperX = cX + CAMERA_VIEW/2;
+		int lowerY = cY - CAMERA_VIEW/2;
+		int upperY = cY + CAMERA_VIEW/2;
+
+		Predicate<Tile> inRange = tile ->
+				tile.getX() >= lowerX && tile.getX() <= upperX && tile.getY() >= lowerY && tile.getY() <= upperY;
+		Stream<Tile> allTiles = game.getMaze().getTiles();
+
+		return allTiles.filter(inRange).collect(Collectors.toList());
 	}
 	
 	public BufferedImage getImageFromObject(GameObject g) {
+		String path = "assets/" + g.getImageString();
 		try {
-			String path = "assets/" + g.getImageString();
 			File imgFile = new File(path);
 			return ImageIO.read(imgFile);
 		} catch (IOException e) {
@@ -70,90 +62,50 @@ public class GameVisuals extends JPanel{
 		return null;
 	}
 	
-	/*
-	private void setImages() {}
-		try {
-			tile_exit = ImageIO.read(new File("assets/tile_exit.png"));
-			tile_exit_lock = ImageIO.read(new File("assets/tile_exit_lock.png"));
-			tile_free = ImageIO.read(new File("assets/tile_free.png"));
-			tile_info = ImageIO.read(new File("assets/tile_info.png"));
-			tile_treasure = ImageIO.read(new File("assets/tile_treasure.png"));
-			tile_wall = ImageIO.read(new File("assets/tile_wall.png"));
-			key_blue = ImageIO.read(new File("assets/key_blue.png"));
-			key_green = ImageIO.read(new File("assets/key_green.png"));
-			key_red = ImageIO.read(new File("assets/key_red.png"));
-			key_yellow = ImageIO.read(new File("assets/key_yellow.png"));
-			locked_blue = ImageIO.read(new File("assets/tile_locked_door_key_blue.png"));
-			locked_green = ImageIO.read(new File("assets/tile_locked_door_key_green.png"));
-			locked_red = ImageIO.read(new File("assets/tile_locked_door_key_red.png"));
-			locked_yellow = ImageIO.read(new File("assets/tile_locked_door_key_yellow.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public int getScreenX(int x) {
+		return x*TILE_SIZE;
 	}
 	
-	private BufferedImage getImg(char tile) {}
-		// list<tiles> 
-		// for(t : tiles) { t.imageString(); }
-		switch(tile) {
-			case 'O':
-				return tile_exit;
-			case 'X':
-				return tile_exit_lock;
-			case '_':
-				return tile_free;
-			case 'i':
-				return tile_info;
-			case '*':
-				return tile_treasure;
-			case '#':
-				return tile_wall;
-		}
-		return null;
-	}
-	*/
-	
-	public int getScreenX(GameObject g) {
-		return g.getX()*TILE_SIZE;
-		
-	}
-	
-	public int getScreenY(GameObject g) {
-		return g.getY()*TILE_SIZE;
+	public int getScreenY(int y) {
+		return y*TILE_SIZE;
 	}
 	
 	public int getTranslateX() {
 		int cX = game.getChap().getX();
-		return cX - CAMERA_VIEW/2;
+		return getScreenX(cX - CAMERA_VIEW/2);
 	}
 	
 	public int getTranslateY() {
 		int cY = game.getChap().getY();
-		return cY - CAMERA_VIEW/2;
+		return getScreenY(cY - CAMERA_VIEW/2);
 	}
 	
 	public void renderTiles(Graphics g) {
-		int transX = getTranslateX();
-		int transY = getTranslateY();
-		g.translate(-transX, -transY);
-		
 		for(Tile t : getTilesToRender()) {
-			int screenX = getScreenX(t);
-			int screenY = getScreenY(t);
+			int screenX = getScreenX(t.getX());
+			int screenY = getScreenY(t.getY());
 			g.drawImage(getImageFromObject(t), screenX, screenY, null);
 		}
-		
-		g.translate(transX, transY);
 	}
 	
 	public void renderChap(Graphics g) {
-		
+		GameObject chap = game.getChap();
+		int x = getScreenX(chap.getX());
+		int y = getScreenY(chap.getY());
+		g.setColor(Color.BLACK);
+		g.fillRect(x, y, 10, 10);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
+		g.setColor(BG_COLOR);
+		g.fillRect(0,0, TILE_SIZE*CAMERA_VIEW, TILE_SIZE*CAMERA_VIEW);
+		int transX = getTranslateX();
+		int transY = getTranslateY();
+		g.translate(-transX, -transY);
 		renderTiles(g);
 		renderChap(g);
+		g.translate(transX, transY);
 	}
 }
  
