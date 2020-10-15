8e28d996d62f4f35b89fc896033d0782d122a6fb
Tama <damien.tamasese@gmail.com>
2020-10-15 13:03:59 +1300

    complete code overhaul, now uses time based approach to identify move pattern. instead of saving game states each move. as can not access game states. all class reworked to account for new approach

--- src/nz/ac/vuw/ecs/swen225/gp30/recnplay/Replay.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/recnplay/Replay.java
@@ -1,16 +1,19 @@
 
 package nz.ac.vuw.ecs.swen225.gp30.recnplay;
 
+import nz.ac.vuw.ecs.swen225.gp30.maze.Move;
 
 import java.util.ArrayList;
 
 public class Replay {
 
     private Boolean autoPlaying = false;
-    private int index = 0;
-    private static int delay = 5000;
+    private int playerIndex = 0;
+    private int actorIndex = 0;
+    private double delay = 1;
 
-    public ArrayList<String> gameStates = new ArrayList<>();
+    public ArrayList<String> playerMoves = new ArrayList<>();
+    public ArrayList<String> actorMoves = new ArrayList<>();
 
     /**
      * Called by the application class, which allows user to select Json file to load,
@@ -19,7 +22,9 @@ public class Replay {
      * @param fileName - user selected filename, see LoadJSON
      */
     public void loadJsonToreplay(String fileName) {
-        gameStates = LoadJSON.loadGameStates(fileName);
+        LoadJSON lj = new LoadJSON();
+        playerMoves = lj.loadPlayerMoves(fileName);
+        actorMoves = lj.loadActorMoves(fileName);
     }
 
     /**
@@ -27,19 +32,29 @@ public class Replay {
      */
     public void setAutoPlaying() {
         autoPlaying = !autoPlaying;
-        delay = 5000;
     }
 
     /**
      * calls the getNextGameState method after a delay whilst auto play function/boolean is true
      */
-    public void autoPlay() {
+    public void autoPlay(int time) {
         while(autoPlaying) {
-            try {
-                Thread.sleep(delay);
-                getNextGameState();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+            int playerMoveTime = convertStringToInt(playerMoves.get(playerIndex));
+                if(playerMoveTime < time){
+                    char move = playerMoves.get(playerIndex).charAt(0);
+                    convertStringTOMove(move);
+                    //send to application to move chap and actor
+                    playerIndex++;
+                    }
+                
+            if(!actorMoves.isEmpty()) {
+                int actorMoveTime = convertStringToInt(actorMoves.get(actorIndex));
+                if(actorMoveTime < time){
+                    char actorMove = actorMoves.get(actorIndex).charAt(0);
+                    convertStringTOMove(actorMove);
+                    //send to application to move chap and actor
+                    actorIndex++;
+                }
             }
         }
     }
@@ -52,12 +67,18 @@ public class Replay {
      *
      * @return String - String that can be read into a gameState
      */
-    public String getNextGameState() {
-        if(index + 1 <= gameStates.size() && !autoPlaying) {
-            index++;
-            return gameStates.get(index);
-        } else {
-            return null;//FIXME replace with error message maybe
+    public void playNextMove() {
+        if(playerIndex + 1 <= playerMoves.size() && !autoPlaying) {
+            playerIndex++;
+            Move playerMove = convertStringTOMove(playerMoves.get(playerIndex).charAt(0));
+            int playerTime = convertStringToInt(playerMoves.get(playerIndex));
+            int actorTime = convertStringToInt(actorMoves.get(actorIndex));
+            if (actorTime < playerTime) {
+                //pass actormove to app
+                actorIndex++;
+            }
+            //pass player move to app
+
         }
     }
 
@@ -68,36 +89,73 @@ public class Replay {
      *
      * @return String - String that can be read into a gameState
      */
-    public String getPreviousGameState() {
-        if(index - 1 >= 0 && !autoPlaying) {
-            index--;
-            return gameStates.get(index);
-        } else {
-            return null;//FIXME replace with error message maybe
+    public void getPreviousMove() {
+        if(playerIndex - 1 >= 0 && !autoPlaying) {
+            playerIndex--;
+            Move playerMove = reverseMove(playerMoves.get(playerIndex).charAt(0));
+            int playerTime = convertStringToInt(playerMoves.get(playerIndex));
+            int actorTime = convertStringToInt(actorMoves.get(actorIndex));
+            if (actorTime > playerTime) {
+                //pass actormove to app
+                Move actorMove = reverseMove(actorMoves.get(actorIndex).charAt(0));
+                actorIndex--;
+            }
+            //pass player move to app
+            playerIndex++;
         }
     }
 
     /**
      * called by application, increases the time between steps on autoplay function
      */
-    public static void increaseDelay(){
-        if(delay < 5000) {
-            delay += 1000;
+    public void decreaseDelay(){
+        if(delay < 4) {
+            delay = delay*2;
         } else {
-            delay = 5000;
+            delay = 4;
         }
     }
 
     /**
      * called by Application, decreases the time between steps on autoplay function
      */
-    public static void decreaseDelay() {
-        if (delay > 1000) {
-            delay -= 1000;
-        } else if (delay >= 1000){
-            delay = 500;
+    public void increaseDelay() {
+        if (delay > .5) {
+            delay = delay/2;
         } else {
-            delay = 250;
+            delay = 0.5;
         }
+    }
+
+    public Move convertStringTOMove(Character s) {
+        switch (s){
+            case 's':
+                return Move.DOWN;
+            case 'd':
+                return Move.RIGHT;
+            case 'a':
+                return Move.LEFT;
+            case 'w':
+                return Move.UP;
+        }
+        return null; // throw error
+    }
+
+    public Move reverseMove(Character s){
+        switch (s){
+            case 'w':
+                return Move.DOWN;
+            case 'a':
+                return Move.RIGHT;
+            case 'd':
+                return Move.LEFT;
+            case 's':
+                return Move.UP;
+        }
+        return null; // throw error
+    }
+
+    public int convertStringToInt(String s){
+        return Integer.parseInt(s,1,s.length(),10);
     }
 }
