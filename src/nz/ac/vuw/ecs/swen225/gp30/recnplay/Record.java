47335ad17fa0bbed3563769c2c2379d87add4502
Tama <damien.tamasese@gmail.com>
2020-10-19 11:44:24 +1300

    Record - readded due to error when committing to git

--- src/nz/ac/vuw/ecs/swen225/gp30/recnplay/Record.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/recnplay/Record.java
@@ -1,70 +1,66 @@
-
 package nz.ac.vuw.ecs.swen225.gp30.recnplay;
 
-import nz.ac.vuw.ecs.swen225.gp30.Move;
 
-import java.util.*;
+import nz.ac.vuw.ecs.swen225.gp30.Move;
+import nz.ac.vuw.ecs.swen225.gp30.maze.tile.Tile;
+
+
+import javax.json.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Record {
 
-    private ArrayList<String> playerActions = new ArrayList<>();
-    private ArrayList<String> playerActionsReplay = new ArrayList<>();
+    //initialises for directory, and standard naming for files
+    private final String dir = "src/nz/ac/vuw/ecs/swen225/gp30/recnplay/";
+    private final String fileNamePrefix = "record-";
+    private final String fileNameSuffix = ".json";
 
-    private ArrayList<String> actorActions = new ArrayList<>();
-    private ArrayList<String> actorActionsReplay = new ArrayList<>();
+    //initialises ints required for saving gameState and file iterations
+    private int saveIteration = 1;
+    private int playerMoveIndex = 0;
+    private int actorMoveIndex =0;
 
-    public void recordPlayerAction(Move action) {
-        String s = convertMoveToString(action);
-        recordActions(s);
+    //initialises Json array builder, file name and file in order to save gameState in an array and write to file
+    private JsonArrayBuilder arrayBuilder;
+    private String fileName;
+    private File file;
+
+    /**
+     * constructor
+     */
+    public WriteJSON() {
+        arrayBuilder = Json.createArrayBuilder();
+        fileName = fileNamePrefix + saveIteration + fileNameSuffix;
+        file = new File(dir + fileName);
     }
 
-    public void recordActorActions(Move action) {
-        String s = convertMoveToString(action);
-        recordActorActions(s);
+    public void storePlayerMove(Move move,int time) {
+        JsonObjectBuilder playerMoveObject = Json.createObjectBuilder().add("Player" + playerMoveIndex++,convertMoveToString(move) + time);
+        arrayBuilder.add(playerMoveObject);
+
     }
 
-    public void recordActions(String a){ //will return an action not void
-        addToPerformedActions(a);
-        addToReplayActions(a);
+    public void storeActorMove(Move move, int time) {
+        JsonObjectBuilder actorMoveObject = Json.createObjectBuilder().add("Actor" + actorMoveIndex++,convertMoveToString(move) + time);
+        arrayBuilder.add(actorMoveObject);
     }
 
-    public void addToReplayActions(String a){
-        String revAct = reverseAction(a);
-        playerActionsReplay.add(revAct);
+    /**
+     * takes a gameState, iterators through it, saves each "tile" as a string.
+     * adds string to a string builder, then creates a Json object using that string.
+     * this json object is then added to the Json array builder
+     *
+     * @param levelNum -String dictating the level number
+     */
+    public void storeLevel(String levelNum) { //FIXME could just load from level
+        JsonObjectBuilder gameStateObj = Json.createObjectBuilder().add("level ", levelNum); //converts level to a JsonObject
+        arrayBuilder.add(gameStateObj); //adds JsonObject to JsonArray
     }
 
-    public void addToPerformedActions(String a){
-        playerActions.add(a);
-    }
-
-    public void recordActorActions(String s){
-        addToActorActions(s);
-        addToActorReplay(s);
-    }
-
-    public void addToActorActions(String a) { actorActions.add(a); }
-
-    public void addToActorReplay(String a) {
-        String revAct = reverseAction(a);
-        actorActionsReplay.add(revAct);
-    }
-
-    public static String reverseAction(String a) {
-        switch (a){
-            case "w":
-                return "s";
-            case "a":
-                return "d";
-            case "s":
-                return "w";
-            case "d":
-                return "a";
-        }
-        return null;
-    }
-
-    public static String convertMoveToString(Move a) {
-        switch (a){
+    public String convertMoveToString(Move a) {
+        switch (a) {
             case DOWN:
                 return "s";
             case RIGHT:
@@ -73,7 +69,55 @@ public class Record {
                 return "w";
             case LEFT:
                 return "a";
+        };
+        return null;
+    }
+
+
+    /**
+     * writes the Json array builder to a Json file
+     * first checks all existing files to makes sure it is not overwriting old files
+     * called when saving a game, or when choosing to watch a replay.
+     */
+    public String writeJsonToFile() {
+        if(file.exists()) {
+            createNewSaveIteration();
         }
-        return null; // throw error
+
+        try {
+            FileWriter fw = new FileWriter(new File(dir + fileName)); //create a file in the correct directory
+            JsonWriter jw = Json.createWriter(fw); //convert to json file
+            jw.writeArray(arrayBuilder.build()); //builds json array and writes to file
+            jw.close();
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+
+    /**
+     * updates the file name to a new iterations i.e. changes the saveIteration
+     */
+    private void createNewSaveIteration() {
+        File recordDir = new File(dir);
+
+        File[] currentRecords = recordDir.listFiles(); //gets all files from the directory
+        if(currentRecords == null){ return; } //if no files exists no need to increase save number
+
+        for(File records : currentRecords) {
+            if(records.getName().contains(fileNameSuffix)){ //checks only files with .json suffixes
+                String stringIterator = records.getName().
+                        substring(records.getName().
+                                indexOf(fileNamePrefix) + 7, records.getName().indexOf(".")); //limits reader to only the integer in the file name
+
+                int fileInt = Integer.parseInt(stringIterator) + 1; //add one to the highest/last file
+                if(fileInt > saveIteration) {
+                    saveIteration = fileInt; //check to make sure highest possible file int will be used
+                }
+            }
+        }
+        fileName = fileNamePrefix + saveIteration + fileNameSuffix;
     }
 }
