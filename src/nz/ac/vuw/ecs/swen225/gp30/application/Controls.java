f51c331eb587d0e7367710121ac3b9f52fffc9b3
jakehobbs <jakehobbs65@gmail.com>
2020-10-21 11:57:06 +1300

    Bug fixes, implemented pause logic correctly.

--- src/nz/ac/vuw/ecs/swen225/gp30/application/Controls.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/application/Controls.java
@@ -150,15 +150,15 @@ public class Controls extends KeyAdapter implements ActionListener {
                 game.playReplay();
                 System.out.println("You are in load file\n");
                 break;
-            case "0.5x speed":
+            case "0.25x speed":
                 //Set speed to 0.5.
                 game.setTimerDelay(4000);
                 System.out.println("You are in 0.25x speed");
-            case "1.0x speed":
+            case "0.5x speed":
                 //Set speed to 1.0.
                 game.setTimerDelay(2000);
                 System.out.println("You are in 0.5x speed");
-            case "1.5x speed":
+            case "1.0x speed":
                 //Set speed to 1.5.
                 game.setTimerDelay(1000);
                 System.out.println("You are in 1.0x speed");
