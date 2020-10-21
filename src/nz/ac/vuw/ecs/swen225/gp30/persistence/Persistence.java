7289a19555230201ff92778f85582ef6c008d53f
Joshua <cookhjosh@ecs.vuw.ac.nz>
2020-10-22 02:51:59 +1300

    Created loadSave method

--- src/nz/ac/vuw/ecs/swen225/gp30/persistence/Persistence.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/persistence/Persistence.java
@@ -10,6 +10,8 @@ import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObjectBuilder;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,8 +23,8 @@ public class Persistence {
 
     public static int NUM_LEVELS = 2;
 
-    public static void main(String args[]){
-        GameWorld g = readLevel(2);
+    public static void main(String args[]) {
+        GameWorld g = loadSave();
         saveGame(g, "name.json");
     }
 
@@ -31,8 +33,8 @@ public class Persistence {
         String gameSave;
 
         StringBuilder iString = new StringBuilder();
-        for (Item i : game.getChap().getInventory()){
-            iString.append(i+"/");
+        for (Item i : game.getChap().getInventory()) {
+            iString.append(i + "/");
         }
         ArrayList<Mob> mobs = (ArrayList<Mob>) game.getMobManager().getMobs();
 
@@ -56,10 +58,10 @@ public class Persistence {
                                     .add("x", mobs.get(0).getX())
                                     .add("y", mobs.get(0).getY())
                                     .add("path", factory.createArrayBuilder()
-                                        .add(mobs.get(0).getPath()[0])
-                                        .add(mobs.get(0).getPath()[1])
-                                        .add(mobs.get(0).getPath()[2])
-                                        .add(mobs.get(0).getPath()[3]))
+                                            .add(mobs.get(0).getPath()[0])
+                                            .add(mobs.get(0).getPath()[1])
+                                            .add(mobs.get(0).getPath()[2])
+                                            .add(mobs.get(0).getPath()[3]))
 
                             )
                             .add("bug-2", factory.createObjectBuilder()
@@ -74,9 +76,9 @@ public class Persistence {
                             .add("boardWidth", game.getMaze().getCols())
                             .add("boardHeight", game.getMaze().getRows())
                             .add("chapChips", game.getChap().getChipsCollected())
+                            .add("chipsRequired", 11)
                             .add("time", game.getTimeLeft()));
-        }
-        else {
+        } else {
             b = Json.createObjectBuilder()
                     .add("levelInfo", game.getLevelInfo())
                     .add("board", game.getMaze().toString(false))
@@ -100,9 +102,9 @@ public class Persistence {
                             .add("boardWidth", game.getMaze().getCols())
                             .add("boardHeight", game.getMaze().getRows())
                             .add("chapChips", game.getChap().getChipsCollected())
+                            .add("chipsRequired", 11)
                             .add("time", game.getTimeLeft()));
         }
-
 
 
         try {
@@ -112,7 +114,7 @@ public class Persistence {
             int saveLength = gameSave.length();
             w.close();
 
-            Writer writer = new BufferedWriter(new FileWriter(fileName));
+            Writer writer = new BufferedWriter(new FileWriter("src/"+fileName));
 
             for (int i = 0; i < saveLength; i++) {
                 char next = gameSave.charAt(i);
@@ -131,128 +133,208 @@ public class Persistence {
     }
 
 
+    public static GameWorld loadSave() {
 
-    public static GameWorld readLevel(int level) {
-        String path = "src/nz/ac/vuw/ecs/swen225/gp30/persistence/levels/level" + level + ".json";
-        try {
-            // create Gson instance
-            Gson gson = new Gson();
+        JFileChooser chooser = new JFileChooser("src/");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "JSON files", "json");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            try {
+                // create Gson instance
+                Gson gson = new Gson();
 
-            // create a reader
-            Reader reader = Files.newBufferedReader(Paths.get(path));
+                // create a reader
+                Reader reader = Files.newBufferedReader(Paths.get(chooser.getSelectedFile().getPath()));
 
-            // convert JSON file to map
-            Map<?, ?> map = gson.fromJson(reader, Map.class);
+                // convert JSON file to map
+                Map<?, ?> map = gson.fromJson(reader, Map.class);
 
-            int width = (int) Double.parseDouble(map.get("boardWidth").toString());
-            int height = (int) Double.parseDouble(map.get("boardHeight").toString());
-            int chipsRequired = (int) Double.parseDouble(map.get("chipsRequired").toString());
-            String boardString = map.get("board").toString();
-            Maze maze = readBoard(chipsRequired, width, height, boardString);
+                int width = (int) Double.parseDouble(map.get("boardWidth").toString());
+                int height = (int) Double.parseDouble(map.get("boardHeight").toString());
+                int chipsRequired = (int) Double.parseDouble(map.get("chipsRequired").toString());
+                String boardString = map.get("board").toString();
+                Maze maze = readBoard(chipsRequired, width, height, boardString);
 
-            int chapX = (int) Double.parseDouble(((Map)map.get("chap")).get("x").toString());
-            int chapY = (int) Double.parseDouble(((Map)map.get("chap")).get("y").toString());
-            Chap chap = new Chap(chapX, chapY);
+                int chapX = (int) Double.parseDouble(((Map) map.get("chap")).get("x").toString());
+                int chapY = (int) Double.parseDouble(((Map) map.get("chap")).get("y").toString());
+                Chap chap = new Chap(chapX, chapY);
 
-            MobManager mobMgr = new MobManager(maze);
-            if(map.containsKey("mobs")) {
-                List<Mob> mobs = readMobs((Map)map.get("mobs"));
-                mobMgr.setMobs(mobs);
+
+                MobManager mobMgr = new MobManager(maze);
+                if (map.containsKey("mobs")) {
+                    List<Mob> mobs = readMobs((Map) map.get("mobs"));
+                    mobMgr.setMobs(mobs);
+                }
+
+                int time;
+                if (map.containsKey("time")) {
+                    time = (int) Double.parseDouble((map.get("time")).toString());
+                }
+
+                int currentChips;
+                if (map.containsKey("chapChips")) {
+                    currentChips = (int) Double.parseDouble((map.get("chapChips")).toString());
+                }
+
+                String inventory;
+                if (map.containsKey("inventory")) {
+                    inventory = map.get("inventory").toString();
+                }
+
+                GameWorld game = new GameWorld(maze, chap);
+                String levelInfo = map.get("levelInfo").toString();
+
+                game.setMobManager(mobMgr);
+                game.setLevelInfo(levelInfo);
+
+                reader.close();
+                return game;
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-
-            GameWorld game = new GameWorld(maze, chap);
-            String levelInfo = map.get("levelInfo").toString();
-
-            game.setMobManager(mobMgr);
-            game.setLevelInfo(levelInfo);
-
-            reader.close();
-            return game;
-        } catch (Exception ex) {
-            ex.printStackTrace();
         }
         return null;
     }
 
-    public static List<Mob> readMobs(Map mobs) {
-        List<Mob> mobsList = new ArrayList<>();
-        int mobX;
-        int mobY;
-        int[] path;
-        for(Object entry : mobs.values()) {
-            mobX = (int) Double.parseDouble(((Map) entry).get("x").toString());
-            mobY = (int) Double.parseDouble(((Map) entry).get("y").toString());
-            List<Double> pathArr = ((List<Double>) ((Map) entry).get("path"));
-            path = new int[pathArr.size()];
-            for(int i=0; i<pathArr.size(); i++) {
-                path[i] = pathArr.get(i).intValue();
-            }
+        public static GameWorld readLevel ( int level){
+            String path = "src/nz/ac/vuw/ecs/swen225/gp30/persistence/levels/level" + level + ".json";
+            try {
+                // create Gson instance
+                Gson gson = new Gson();
 
-            mobsList.add(new Bug(mobX, mobY, path));
+                // create a reader
+                Reader reader = Files.newBufferedReader(Paths.get(path));
+
+                // convert JSON file to map
+                Map<?, ?> map = gson.fromJson(reader, Map.class);
+
+                int width = (int) Double.parseDouble(map.get("boardWidth").toString());
+                int height = (int) Double.parseDouble(map.get("boardHeight").toString());
+                int chipsRequired = (int) Double.parseDouble(map.get("chipsRequired").toString());
+                String boardString = map.get("board").toString();
+                Maze maze = readBoard(chipsRequired, width, height, boardString);
+
+                int chapX = (int) Double.parseDouble(((Map) map.get("chap")).get("x").toString());
+                int chapY = (int) Double.parseDouble(((Map) map.get("chap")).get("y").toString());
+                Chap chap = new Chap(chapX, chapY);
+
+
+                MobManager mobMgr = new MobManager(maze);
+                if (map.containsKey("mobs")) {
+                    List<Mob> mobs = readMobs((Map) map.get("mobs"));
+                    mobMgr.setMobs(mobs);
+                }
+
+                int time;
+                if (map.containsKey("time")) {
+                    time = (int) Double.parseDouble((map.get("time")).toString());
+                }
+
+                int currentChips;
+                if (map.containsKey("chapChips")) {
+                    currentChips = (int) Double.parseDouble((map.get("chapChips")).toString());
+                }
+
+                String inventory;
+                if (map.containsKey("inventory")) {
+                    inventory = map.get("inventory").toString();
+                }
+
+                GameWorld game = new GameWorld(maze, chap);
+                String levelInfo = map.get("levelInfo").toString();
+
+                game.setMobManager(mobMgr);
+                game.setLevelInfo(levelInfo);
+
+                reader.close();
+                return game;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
         }
 
-        return mobsList;
-    }
+        public static List<Mob> readMobs (Map mobs){
+            List<Mob> mobsList = new ArrayList<>();
+            int mobX;
+            int mobY;
+            int[] path;
+            for (Object entry : mobs.values()) {
+                mobX = (int) Double.parseDouble(((Map) entry).get("x").toString());
+                mobY = (int) Double.parseDouble(((Map) entry).get("y").toString());
+                List<Double> pathArr = ((List<Double>) ((Map) entry).get("path"));
+                path = new int[pathArr.size()];
+                for (int i = 0; i < pathArr.size(); i++) {
+                    path[i] = pathArr.get(i).intValue();
+                }
 
-    public static Maze readBoard(int chipsRequired, int width, int height, String board){
-        Maze m = new Maze(width, height);
-
-        Scanner sc = new Scanner(board).useDelimiter(",");
-        int x = 0;
-        int y = 0;
-
-        while(sc.hasNext()){
-            String tile = sc.next();
-            switch (tile) {
-                case "_":
-                    m.setTileAt(x, y, new FreeTile(x, y));
-                    break;
-                case "#":
-                    m.setTileAt(x, y, new WallTile(x, y));
-                    break;
-                case "T":
-                    m.setTileAt(x, y, new TreasureTile(x, y));
-                    break;
-                case "E":
-                    m.setTileAt(x, y, new ExitTile(x, y));
-                    break;
-                case "G":
-                    m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_GREEN));
-                    break;
-                case "g":
-                    m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_GREEN));
-                    break;
-                case "R":
-                    m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_RED));
-                    break;
-                case "r":
-                    m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_RED));
-                    break;
-                case "B":
-                    m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_BLUE));
-                    break;
-                case "b":
-                    m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_BLUE));
-                    break;
-                case "Y":
-                    m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_YELLOW));
-                    break;
-                case "y":
-                    m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_YELLOW));
-                    break;
-                case "i":
-                    m.setTileAt(x, y, new InfoTile(x, y));
-                    break;
-                case "l":
-                    m.setTileAt(x, y, new ExitLockTile(x, y, chipsRequired));
-                    break;
+                mobsList.add(new Bug(mobX, mobY, path));
             }
-            x++;
-            if(x == width){
-                y++;
-                x = 0;
-            }
+
+            return mobsList;
         }
-        return m;
+
+        public static Maze readBoard ( int chipsRequired, int width, int height, String board){
+            Maze m = new Maze(width, height);
+
+            Scanner sc = new Scanner(board).useDelimiter(",");
+            int x = 0;
+            int y = 0;
+
+            while (sc.hasNext()) {
+                String tile = sc.next();
+                switch (tile) {
+                    case "_":
+                        m.setTileAt(x, y, new FreeTile(x, y));
+                        break;
+                    case "#":
+                        m.setTileAt(x, y, new WallTile(x, y));
+                        break;
+                    case "T":
+                        m.setTileAt(x, y, new TreasureTile(x, y));
+                        break;
+                    case "E":
+                        m.setTileAt(x, y, new ExitTile(x, y));
+                        break;
+                    case "G":
+                        m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_GREEN));
+                        break;
+                    case "g":
+                        m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_GREEN));
+                        break;
+                    case "R":
+                        m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_RED));
+                        break;
+                    case "r":
+                        m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_RED));
+                        break;
+                    case "B":
+                        m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_BLUE));
+                        break;
+                    case "b":
+                        m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_BLUE));
+                        break;
+                    case "Y":
+                        m.setTileAt(x, y, new LockedDoorTile(x, y, Item.KEY_YELLOW));
+                        break;
+                    case "y":
+                        m.setTileAt(x, y, new KeyTile(x, y, Item.KEY_YELLOW));
+                        break;
+                    case "i":
+                        m.setTileAt(x, y, new InfoTile(x, y));
+                        break;
+                    case "l":
+                        m.setTileAt(x, y, new ExitLockTile(x, y, chipsRequired));
+                        break;
+                }
+                x++;
+                if (x == width) {
+                    y++;
+                    x = 0;
+                }
+            }
+            return m;
+        }
     }
-}
