ee1fec36544a6094221f8cef7225e7f145e3f20a
ogcamplin <39985491+ogcamplin@users.noreply.github.com>
2020-10-23 00:49:37 +1300

    Added javadocs

---
+++ src/nz/ac/vuw/ecs/swen225/gp30/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Main package for the Chaps Challenge game implementation.
+ *
+ * @author Oscar Camplin, Jake Hobbs, Damien Tamasese, Joshua Cook, Pranav Gohil
+ */
+package nz.ac.vuw.ecs.swen225.gp30;
