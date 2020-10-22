b8322ce9166c971eb4c4526fdfc989341ba84d95
Tama <damien.tamasese@gmail.com>
2020-10-23 09:52:58 +1300

    reverted level back to private error was in how save save function was being called

--- src/nz/ac/vuw/ecs/swen225/gp30/recnplay/Replay.java
+++ src/nz/ac/vuw/ecs/swen225/gp30/recnplay/Replay.java
@@ -19,8 +19,7 @@ public class Replay {
     private Boolean autoPlaying = false;
     private int playerIndex = 0;
     private int delay = 1;
-
-    public int level; //made public as getter method wasn't working correctly
+    private int level;
 
 
     /**
@@ -109,6 +108,16 @@ public class Replay {
     public void resetAutoPlay() {
         autoPlaying = false;
         playerIndex = 0;
+        delay =1;
+    }
+
+    /**
+     * returns the level of the replay as an int for loading purposes
+     *
+     * @return level - int representing level number
+     */
+    public int getReplayLevel() {
+        return level;
     }
 
     /**
